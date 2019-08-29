eb3dd9b05b1f89affd763fe5dff1dad1ae6421db
divya-govind <divya.p@nabler.com>
2019-08-30 00:27:55 +0530

    first commit

---
+++ lambda/bibot_config.py
@@ -0,0 +1,42 @@
+#
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+ORIGINAL_VALUE = 0
+TOP_RESOLUTION = 1
+
+SLOT_CONFIG = {
+    'productline':       {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find an event called "{}".'},
+       {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find a category called "{}".'},
+    'count':            {'type': ORIGINAL_VALUE, 'remember': True},
+    'dimension':        {'type': ORIGINAL_VALUE, 'remember': True},
+    'one_productline':        {'type': TOP_RESOLUTION, 'remember': False, 'error': 'I couldn\'t find an event called "{}".'},
+    'another_productline':    {'type': TOP_RESOLUTION, 'remember': False, 'error': 'I couldn\'t find an event called "{}".'}
+}
+
+DIMENSIONS = {
+    'productline':     {'slot': 'productline',  'column': 'productline',  'singular': 'sales'},
+    'status':     {'slot': 'status', 'column': 'status',       'singular': 'sales'},
+    'date':     {'slot': 'date',  'column': 'orderdate',  'singular': 'sales'},
+    'city':     {'slot': 'city',  'column': 'city',  'singular': 'sales'},
+    'state':     {'slot': 'state', 'column': 'state', 'singular': 'sales'},
+    'country': {'slot': 'country',    'column': 'country',    'singular': 'sales'}
+}
+
+
+class SlotError(Exception):
+    pass
+
